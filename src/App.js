1edc92182739a4c77dc8300a31618e4bb5674ef5
Zac Xu <zacfirst@gmail.com>
2019-07-27 00:07:24 +1000

    feat: add drawer component

--- src/App.js
+++ src/App.js
@@ -1,86 +1,11 @@
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Container from '@material-ui/core/Container';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import React, { useState } from 'react';
-import { getCustomer } from './api/userApi';
+import React from 'react';
 import './App.css';
-import CustomerCard from './customer';
-
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(12, 1fr)',
-    gridGap: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    whiteSpace: 'nowrap',
-    marginBottom: theme.spacing(1),
-  },
-  divider: {
-    margin: theme.spacing(2, 0),
-  },
-}));
+import Drawer from './Drawer';
 
 export default function App() {
-  const [user, setUser] = useState(null);
-  getCustomer(1)
-    .then(response => {
-      setUser(response);
-    });
-  const classes = useStyles();
-
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <Grid container spacing={3}>
-          <Grid item xs={5}>
-            <Paper className={classes.paper}>xs=5</Paper>
-            <header className="App-header">
-              <CustomerCard user={user} />
-            </header>
-          </Grid>
-          <Grid item xs={2}>
-            <Paper className={classes.paper}>xs=2</Paper>
-            <Grid container spacing={1} direction="column" alignItems="center">
-              <Grid item>
-                <ButtonGroup variant="contained" size="small" aria-label="Small contained button group">
-                  <Button>One</Button>
-                  <Button>Two</Button>
-                  <Button>Three</Button>
-                </ButtonGroup>
-              </Grid>
-              <Grid item>
-                <ButtonGroup variant="contained" size="small" aria-label="Small contained button group">
-                  <Button>One</Button>
-                  <Button>Two</Button>
-                  <Button>Three</Button>
-                </ButtonGroup>
-              </Grid>
-              <Grid item>
-                <ButtonGroup variant="contained" size="small" aria-label="Small contained button group">
-                  <Button>One</Button>
-                  <Button>Two</Button>
-                  <Button>Three</Button>
-                </ButtonGroup>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xs={5}>
-            <Paper className={classes.paper}>xs=5</Paper>
-            <header className="App-header">
-              <CustomerCard user={user} />
-            </header>
-          </Grid>
-        </Grid>
-      </Container>
+      <Drawer />
   </React.Fragment>
   );
 }
